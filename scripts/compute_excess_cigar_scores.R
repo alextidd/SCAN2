257a2be5e99cd5f34a459e8b8eb3cb891a5e9dad
alextidd <at31@sanger.ac.uk>
2026-02-05 09:54:00 +0000

    add debug logging to R scripts and increase memory allocation in Snakemake rules

--- scripts/compute_excess_cigar_scores.R
+++ scripts/compute_excess_cigar_scores.R
@@ -1,12 +1,21 @@
 #!/usr/bin/env Rscript
 
+# Debug: Print environment info immediately
+cat("=== DEBUG START ===\n", file=stderr())
+cat("TMPDIR env:", Sys.getenv("TMPDIR"), "\n", file=stderr())
+cat("tempdir():", tempdir(), "\n", file=stderr())
+cat("getwd():", getwd(), "\n", file=stderr())
+
 # detect script being run by snakemake
 # if so, make a mock commandArgs function
 if ('snakemake' %in% ls()) {
+    cat("DEBUG: Running via snakemake\n", file=stderr())
     logfile <- snakemake@log[[1]]
+    cat("DEBUG: logfile =", logfile, "\n", file=stderr())
     con <- file(logfile, 'w')
     sink(con, type='output')
     sink(con, type='message')
+    cat("DEBUG: Sinks set up successfully\n")
 
     commandArgs <- function(...) {
         ret <- unlist(c(
@@ -27,11 +36,13 @@ if ('snakemake' %in% ls()) {
     print(commandArgs())
 }
 
+cat("DEBUG: Parsing command line arguments...\n")
 args <- commandArgs(trailingOnly=TRUE)
 if (length(args) < 9) {
     stop("usage: compute_ab_ests_and_models.R config.yaml single.cell.ID integrated_table.tab.gz sccigars.tab.gz bulkcigars.tab.gz cigardata.tab.gz out.tab out.tab.gz n.cores [ chromosomes ]")
 }
 
+cat("DEBUG: Assigning variables from args...\n")
 config.yaml <- args[1]
 single.cell <- args[2]
 int.tab <- args[3]
@@ -42,30 +53,49 @@ out.tab <- args[7]
 out.tab.gz <- args[8]
 n.cores <- as.integer(args[9])
 
-chroms <- c()
-if (length(args) > 9)
-    chroms <- args[-(1:9)]
+cat("DEBUG: config.yaml =", config.yaml, "\n")
+cat("DEBUG: single.cell =", single.cell, "\n")
+cat("DEBUG: int.tab =", int.tab, "\n")
+cat("DEBUG: sccigars.path =", sccigars.path, "\n")
+cat("DEBUG: bulkcigars.path =", bulkcigars.path, "\n")
+cat("DEBUG: trainingcigars.path =", trainingcigars.path, "\n")
+cat("DEBUG: out.tab =", out.tab, "\n")
+cat("DEBUG: out.tab.gz =", out.tab.gz, "\n")
+cat("DEBUG: n.cores =", n.cores, "\n")
 
+chroms <- c()
+if (length(args) > 9) {
+    chroms <- args[-(1:9)]
+    cat("DEBUG: chroms =", chroms, "\n")
+}
+
+cat("DEBUG: Checking if output files already exist...\n")
 for (f in c(out.tab, out.tab.gz, paste0(out.tab.gz, '.tbi'))) {
     if (file.exists(f))
         stop(paste('output file', f, 'already exists, please delete it first'))
 }
 
+cat("DEBUG: Loading library scan2...\n")
 suppressMessages(library(scan2))
+cat("DEBUG: Loading library future...\n")
 suppressMessages(library(future))
+cat("DEBUG: Loading library progressr...\n")
 suppressMessages(library(progressr))
 
-if (n.cores > 1) {
-    plan(multicore, workers=n.cores)
-} else {
-    plan(sequential)
-}
+cat("DEBUG: Setting up plan with", n.cores, "cores...\n")
+# Force sequential to debug the error
+cat("DEBUG: FORCING SEQUENTIAL PLAN FOR DEBUGGING\n")
+plan(sequential)
 
+cat("DEBUG: Creating scan object with make.scan()...\n")
 object <- make.scan(config.path=config.yaml, single.cell=single.cell)
+cat("DEBUG: make.scan() completed\n")
 
 # Restrict the analysis set to a set of chromosomes, if specified
-if (length(chroms) > 0)
+if (length(chroms) > 0) {
+    cat("DEBUG: Restricting analysis regions to chromosomes:", chroms, "\n")
     object@analysis.regions <- object@analysis.regions[seqnames(object@analysis.regions) %in% chroms,]
+}
 print(object@analysis.regions)
 
 # Use fewer tiles when run on individual chromosomes to avoid unnecessary
@@ -75,26 +105,46 @@ if (length(chroms) > 0) {
     target.tile.number <- 100
 }
 
-cat('target.tile.number', target.tile.number, '\n')
+cat('DEBUG: target.tile.number =', target.tile.number, '\n')
+
+cat("DEBUG: Computing parallelization tiles...\n")
+grs.for.para <- analysis.set.tiling.for.parallelization(object, total.tiles=target.tile.number)
+cat("DEBUG: Parallelization tiles computed\n")
+
+cat("DEBUG: Starting run.chunked.pipeline with what.to.compute='excess.cigar'...\n")
+cat("DEBUG: int.tab =", int.tab, "\n")
+cat("DEBUG: sccigars.path =", sccigars.path, "\n")
+cat("DEBUG: bulkcigars.path =", bulkcigars.path, "\n")
+cat("DEBUG: trainingcigars.path =", trainingcigars.path, "\n")
 
 with_progress({
     handlers(handler_newline())
     object <- run.chunked.pipeline(object=object, int.tab=int.tab,
         sccigars=sccigars.path, bulkcigars=bulkcigars.path, trainingcigars=trainingcigars.path,
-        grs.for.parallelization=analysis.set.tiling.for.parallelization(object, total.tiles=target.tile.number),
+        grs.for.parallelization=grs.for.para,
         what.to.compute='excess.cigar',
         verbose=FALSE, report.mem=TRUE)
 }, enable=TRUE)
+cat("DEBUG: run.chunked.pipeline completed\n")
 
 print('hi')
 print(object@gatk)
 
-# hack to make the first line a #-initiated header line for tabix
+cat("DEBUG: Writing results to", out.tab, "\n")
 colnames(object@gatk)[1] <- '#chr'
 data.table::fwrite(object@gatk[,.(`#chr`,pos,refnt,altnt,muttype,id.score,hs.score)],
     file=out.tab, sep='\t', quote=FALSE)
+cat("DEBUG: fwrite completed\n")
+
+cat("DEBUG: Running bgzip on", out.tab, "->", out.tab.gz, "\n")
 Rsamtools::bgzip(out.tab, out.tab.gz)
+cat("DEBUG: bgzip completed\n")
+
+cat("DEBUG: Running indexTabix on", out.tab.gz, "\n")
 Rsamtools::indexTabix(file=out.tab.gz, format='vcf', comment='#')
+cat("DEBUG: indexTabix completed\n")
+
+cat("DEBUG: Script completed successfully\n")
 
 if ('snakemake' %in% ls()) {
     sink()
--- snakemake/scripts/abmodel_covariates.R
+++ snakemake/scripts/abmodel_covariates.R
@@ -1,12 +1,25 @@
 #!/usr/bin/env Rscript
 
+# Debug: Print environment info to stderr immediately
+cat("=== DEBUG START ===\n", file=stderr())
+cat("TMPDIR env:", Sys.getenv("TMPDIR"), "\n", file=stderr())
+cat("tempdir():", tempdir(), "\n", file=stderr())
+cat("R_HOME:", Sys.getenv("R_HOME"), "\n", file=stderr())
+cat("getwd():", getwd(), "\n", file=stderr())
+
 # detect script being run by snakemake
 # if so, make a mock commandArgs function
 if ('snakemake' %in% ls()) {
+    cat("DEBUG: Running via snakemake\n", file=stderr())
     logfile <- snakemake@log[[1]]
+    cat("DEBUG: logfile =", logfile, "\n", file=stderr())
+    cat("DEBUG: Opening log file connection...\n", file=stderr())
     con <- file(logfile, 'w')
+    cat("DEBUG: Connection opened, setting up sink for output...\n", file=stderr())
     sink(con, type='output')
+    cat("DEBUG: Output sink set, setting up sink for message...\n", file=stderr())
     sink(con, type='message')
+    cat("DEBUG: Both sinks set up successfully\n")
 
     commandArgs <- function(...) {
         ret <- unlist(c(
@@ -25,11 +38,13 @@ if ('snakemake' %in% ls()) {
     print(commandArgs())
 }
 
+cat("DEBUG: Parsing command line arguments...\n")
 args <- commandArgs(trailingOnly=TRUE)
 if (length(args) != 8) {
     stop("usage: spatial_sens_abmodel.R integrated_table.tab.gz abfits.rda regions.bed.gz singlecell_id genome_string out.tab out.tab.gz n_cores")
 }
 
+cat("DEBUG: Assigning variables from args...\n")
 integrated.table.path <- args[1]
 abfits.path <- args[2]
 bed.path <- args[3]
@@ -39,31 +54,55 @@ out.tab <- args[6]
 out.tab.gz <- args[7]
 n.cores <- as.integer(args[8])
 
+cat("DEBUG: integrated.table.path =", integrated.table.path, "\n")
+cat("DEBUG: abfits.path =", abfits.path, "\n")
+cat("DEBUG: bed.path =", bed.path, "\n")
+cat("DEBUG: sc.sample =", sc.sample, "\n")
+cat("DEBUG: genome.string =", genome.string, "\n")
+cat("DEBUG: out.tab =", out.tab, "\n")
+cat("DEBUG: out.tab.gz =", out.tab.gz, "\n")
+cat("DEBUG: n.cores =", n.cores, "\n")
+
+cat("DEBUG: Checking if output files already exist...\n")
 for (f in c(out.tab, out.tab.gz))
     if (file.exists(f))
         stop(paste('output file', f, 'already exists, please delete it first'))
 
+cat("DEBUG: Loading library scan2...\n")
 suppressMessages(library(scan2))
+cat("DEBUG: Loading library Rsamtools...\n")
 suppressMessages(library(Rsamtools))
+cat("DEBUG: Loading library future...\n")
 suppressMessages(library(future))
+cat("DEBUG: Loading library progressr...\n")
 suppressMessages(library(progressr))
+cat("DEBUG: Setting up multicore plan with", n.cores, "workers...\n")
 plan(multicore, workers=n.cores)
 
+cat("DEBUG: Loading ab.fits from", abfits.path, "...\n")
 ab.fits <- get(load(abfits.path))
+cat("DEBUG: ab.fits loaded successfully\n")
 
-sens.regions <- data.table::fread(bed.path)
+cat("DEBUG: Reading sens.regions from", bed.path, "...\n")
+sens.regions <- data.table::fread(cmd = paste("zcat", bed.path))
 colnames(sens.regions) <- c('chr', 'start', 'end')
 sens.regions$end <- sens.regions$end - 1
+cat("DEBUG: sens.regions has", nrow(sens.regions), "rows\n")
 
+cat("DEBUG: Creating GRanges object...\n")
 gr.sens.regions <- GenomicRanges::GRanges(seqnames=sens.regions$chr,
     ranges=IRanges::IRanges(start=sens.regions$start, end=sens.regions$end),
     seqinfo=genome.string.to.seqinfo.object(genome.string))
+cat("DEBUG: GRanges object created\n")
 
+cat("DEBUG: Creating parallelization tiles...\n")
 # Don't want that many tiles here; each one causes two extra tabix reads.
 grs.for.para <- analysis.set.tiling.for.parallelization.helper(
     regions=GenomicRanges::reduce(gr.sens.regions),
     total.tiles=10)
+cat("DEBUG: Parallelization tiles created\n")
 
+cat("DEBUG: Starting compute.spatial.sensitivity.abmodel...\n")
 with_progress({
     handlers(handler_newline())
     abmodel <- compute.spatial.sensitivity.abmodel(
@@ -74,13 +113,22 @@ with_progress({
         grs.for.parallelization=grs.for.para,
         genome.string=genome.string)
 }, enable=TRUE)
+cat("DEBUG: compute.spatial.sensitivity.abmodel completed\n")
 
-cat("Writing results to", out.tab, "\n")
+cat("DEBUG: Writing results to", out.tab, "\n")
 colnames(abmodel)[1] <- "#chr"
 data.table::fwrite(abmodel, file=out.tab, sep="\t", col.names=TRUE)
+cat("DEBUG: fwrite completed\n")
 
+cat("DEBUG: Running bgzip on", out.tab, "->", out.tab.gz, "\n")
 Rsamtools::bgzip(out.tab, out.tab.gz)
+cat("DEBUG: bgzip completed\n")
+
+cat("DEBUG: Running indexTabix on", out.tab.gz, "\n")
 Rsamtools::indexTabix(file=out.tab.gz, format='bed', comment='#')
+cat("DEBUG: indexTabix completed\n")
+
+cat("DEBUG: Script completed successfully\n")
 
 if ('snakemake' %in% ls()) {
     sink()
